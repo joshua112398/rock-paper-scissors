365524685fb3c4f5dcae1025a6bd9dfc81086573
Josh Geronimo <joshuageronimo112398@gmail.com>
2022-05-09 21:29:36 -0700

    Add scoring system and end result popup

--- scripts/script.js
+++ scripts/script.js
@@ -14,11 +14,13 @@ function computerPlay() {
   }
 }
 
+////////////////
+
 // Generates a single round and determines the winner based on the 
 // selection of the player and the randomly generated choice of the
 // computer.
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection, playerScore, computerScore) {
 
   const playerChoice = document.querySelector(".playerChoice");
   const computerChoice = document.querySelector(".computerChoice");
@@ -39,59 +41,123 @@ function playRound(playerSelection, computerSelection) {
   });
 
   let result = findWinner(playerSelection, computerSelection);
-  const winner = document.querySelector(".winner");
-  winner.textContent = result;
+  if (result === 'player') {
+    playerScore += 1;
+  }
+  else if (result === 'computer') {
+    computerScore += 1;
+  }
+
+  const playerScoreboard = document.querySelector(".playerScore");
+  const computerScoreboard = document.querySelector(".computerScore");
+  
+  setScore(playerScoreboard, playerScore);
+  setScore(computerScoreboard, computerScore);
+
+  return result;
+
 }
 
+/////////////////
+
+function setScore(scoreboard, score) {
+  const scoreBoxes = scoreboard.children;
+  for (let i = 0; i < score && score <= 5; i++) {
+    if (scoreBoxes[i].style.backgroundColor !== 'black') {
+      scoreBoxes[i].style.backgroundColor = 'black';
+    }
+  }
+}
+
+/////////////////
+
 function findWinner(playerSelection, computerSelection) {
+  const winner = document.querySelector(".winner");
+  let winnerName = "";
   if (playerSelection === 'rock') {
     switch (computerSelection) {
       case 'rock':
-        return "You tied!";
+        winner.textContent = "You tied!";
+        winnerName = "none";
+        break;
       case 'paper':
-        return "You lose! Paper beats Rock.";
+        winner.textContent = "You lose! Paper beats Rock.";
+        winnerName = "computer";
+        break;
       default:
-        return "You win! Rock beats Scissors.";
+        winner.textContent = "You win! Rock beats Scissors.";
+        winnerName = "player";
+        break;
     }
   } else if (playerSelection === 'paper') {
     switch (computerSelection) {
       case 'rock':
-        return "You win! Paper beats Rock.";
+        winner.textContent = "You win! Paper beats Rock.";
+        winnerName = "player";
+        break;
       case 'paper':
-        return "You tied!";
+        winner.textContent = "You tied!";
+        winnerName = "none";
+        break;
       default:
-        return "You lose! Scissors beats Paper.";
+        winner.textContent = "You lose! Scissors beats Paper.";
+        winnerName = "computer";
+        break;
     }
   } else if (playerSelection === 'scissors') {
     switch (computerSelection) {
       case 'rock':
-        return "You lose! Rock beats Scissors.";
+        winner.textContent = "You lose! Rock beats Scissors.";
+        winnerName = "computer";
+        break;
       case 'paper':
-        return "You win! Scissors beats Paper.";
+        winner.textContent = "You win! Scissors beats Paper.";
+        winnerName = "player";
+        break;
       default:
-        return "You tied!";
+        winner.textContent = "You tied!";
+        winnerName = "none";
+        break;
     }
   }
+
+  return winnerName;
 }
+
+/////////////////
 
 // Starts the actual game and takes input from the user each round
 function game() {
 
-  // Plays 5 rounds of the game
-  /*
-  for (let i = 0; i < 5; i++) {
-    let userInput = prompt("Rock, Paper, or Scissors?");
-    let result = playRound(userInput, computerPlay());
-    console.log(result);
-    alert(result);
-  }
-  */
+  let playerScore = 0;
+  let computerScore = 0;
+  let result = "";
 
-  const buttons = document.querySelectorAll("button");
+  const buttons = document.querySelectorAll(".choices button");
   buttons.forEach((button) => {
     button.addEventListener('click', () => {
-      playRound(button.getAttribute('class'), computerPlay());
+      result = playRound(button.getAttribute('class'), computerPlay(), playerScore, computerScore);
+      if (result === 'player') {
+        playerScore += 1;
+      } else if (result === 'computer') {
+        computerScore += 1;
+      }
+
+
+      const modal = document.querySelector(".modal");
+      if (playerScore === 5) {
+        modal.lastElementChild.firstElementChild.textContent = 'YOU WON!';
+        modal.style.display = "block";
+      } else if (computerScore === 5) {
+        modal.lastElementChild.firstElementChild.textContent = 'YOU LOST!';
+        modal.style.display = "block";
+      }
     });
+  });
+
+  const playAgain = document.querySelector(".playAgain");
+  playAgain.addEventListener('click', () => {
+    window.location.reload();
   });
 
 }
